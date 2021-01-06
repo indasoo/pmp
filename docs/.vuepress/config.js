81a21961348d38fd3a34cf0a2502b84df2b2a051
曾银田 <zengyintian@cmiot.chinamobile.com>
2021-01-06 10:30:20 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+const navConf = require("./config/navConf");
+const headConf = require("./config/headConf");
+const pluginConf = require("./config/pluginConf");
+
+// https://vuepress.vuejs.org/guide/basic-config.html#config-file
+module.exports = {
+
+  // ↓↓↓↓↓↓↓↓↓根据实际情况自行配置↓↓↓↓↓↓↓↓↓
+  title: '',  // 
+  description: '',
+  base: '/', 
+  // ↑↑↑↑↑↑↑↑↑根据实际情况自行配置↑↑↑↑↑↑↑↑↑
+  
+  head: headConf,
+  plugins: pluginConf,
+ 
+  themeConfig: {
+    // https://vuepress.vuejs.org/theme/default-theme-config.html
+    nav: navConf,
+
+    // ↓↓↓↓↓↓↓↓↓根据实际情况自行配置↓↓↓↓↓↓↓↓↓
+    repo: '', 
+    // ↑↑↑↑↑↑↑↑↑根据实际情况自行配置↑↑↑↑↑↑↑↑↑
+    editLinks: true,
+    docsDir: 'docs',
+    docsBranch: 'master',
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+  }
+}
---
+++ docs/.vuepress/config/navConf.js
@@ -0,0 +1,4 @@
+module.exports = [
+    // https://vuepress.vuejs.org/theme/default-theme-config.html#navbar-links
+    {name: '首页', link: '/'},
+];
