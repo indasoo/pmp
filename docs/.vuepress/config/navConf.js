b2b26c64d10eea7b6bfe947e0250be55e26b318f
曾银田 <zengyintian@cmiot.chinamobile.com>
2021-01-29 14:49:26 +0800

    增加十五至尊图英文版

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -1,5 +1,6 @@
 module.exports = [
     // https://vuepress.vuejs.org/theme/default-theme-config.html#navbar-links
     {text: '首页', link: '/'},
+    {text: 'PMBOK-V6', link: '/pmbok-v6/0000-pmbok-en'},
     {text: '备考计划', link: '/study-plan'},
 ];
