3e21d9bc61eb1a0818d477d84b979a063323cecf
曾银田 <zengyintian@cmiot.chinamobile.com>
2021-01-29 15:01:58 +0800

    rename pmbok

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -1,6 +1,6 @@
 module.exports = [
     // https://vuepress.vuejs.org/theme/default-theme-config.html#navbar-links
     {text: '首页', link: '/'},
-    {text: 'PMBOK-V6', link: '/pmbok-v6/0000-pmbok-en'},
+    {text: 'PMBOK-V6', link: '/pmbok-en/0000-pmbok'},
     {text: '备考计划', link: '/study-plan'},
 ];
